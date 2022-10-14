884974e7c98248a89d1c92ee28cd50a3c19170cb
hanna mzeget <elimzghanna123@gmail.com>
2022-10-14 23:54:42 +0300

    SADASDFASVRERWVWE

--- script/main.js
+++ script/main.js
@@ -69,7 +69,7 @@ let counter = 0;
                 let svg = document.createElement("svg");
                 svg.classList.add("currentColor");
                 svg.setAttribute("fill", "currentColor");
-                svg.setAttribute("xmlns", "http://www.w3.org/2000/svg%22%22)
+                svg.setAttribute("xmlns", "http://www.w3.org/2000/svg%22%22")
                 let path = document.createElement("path");
                 path.setAttribute("d", "M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z");
 
