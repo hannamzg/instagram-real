baf6817c5846a74631479127cb21729cd6b0b6f8
hanna mzeget <elimzghanna123@gmail.com>
2022-10-19 00:47:42 +0300

    dsadda

--- script/main.js
+++ script/main.js
@@ -11,8 +11,6 @@ let base_api = `https://www.breakingbadapi.com/api/`;
 let maxScroll = 0;
 
 
-
-
 async function getNames(){
     let theApi = `${base_api}character/random`
     for (let i = 0; i < 16;i++){
@@ -24,13 +22,11 @@ async function getNames(){
     
 }
 
-
-
 getNames()
 
 function buildTheStories(){
     let stories  = mangerStroy.getTheStoryes();
-    let stamArr = []
+    let stamArr = [];
     let html = "";
 
     for(let key of stories){
@@ -74,15 +70,13 @@ function stam()
                 console.log(stories[getTheIdAfterNumber]);
                 let div2 = document.createElement("div");
                 div2.classList.add("closeStoryOpen");
-                let svg = document.createElement("svg");
-                svg.classList.add("currentColor");
-                svg.setAttribute("fill", "currentColor");
-                svg.setAttribute("xmlns", "http://www.w3.org/2000/svg%22%22")
-                let path = document.createElement("path");
-                path.setAttribute("d", "M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z");
-
-                svg.appendChild(path);
-                div2.appendChild(svg);
+                let iRight = document.createElement("i");
+                iRight.style.color = "blue";
+                iRight.style.fontSize ="50px"
+                iRight.classList.add("bi")
+                iRight.classList.add("bi-arrow-right")
+                
+                div2.appendChild(iRight);
                 div1.appendChild(img);
                 div1.appendChild(div2);
                 body.appendChild(div1);
@@ -93,76 +87,6 @@ function stam()
           });
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* function stam(){
-    let stories  = mangerStroy.getTheStoryes();
-    let divHaveStoryAndName = document.getElementsByClassName("divHaveStoryAndName");
-    let body = document.querySelector("body");
-
-for(let i=0 ; i<divHaveStoryAndName.length; i++){
-    divHaveStoryAndName[i].addEventListener("click" ,(e)=>{
-        for(let j=0; j<stories.length; j++){
-        let found = e.path[1].id !== stories[j].id;
-        let theTarget = e.path[0];
-            if(found){
-                let div1 = document.createElement("div");
-                div1.classList.add("theStroyOpen");
-                let img = document.createElement("img");
-                img.classList.add("photoStoryOpen");
-                img.src = theTarget.src;
-                let div2 = document.createElement("div");
-                div2.classList.add("closeStoryOpen");
-                let svg = document.createElement("svg");
-                svg.classList.add("currentColor");
-                svg.setAttribute("fill", "currentColor");
-                svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-                let path = document.createElement("path");
-                path.setAttribute("d", "M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z");
-
-                svg.appendChild(path);
-                div2.appendChild(svg);
-                div1.appendChild(img);
-                div1.appendChild(div2);
-                body.appendChild(div1);
-                    setTimeout(() => {
-                        div1.style.display = "none"
-                    }, 2000);
-                }
-        }
-      })
-
-
-
-
-
-    }
-}
- */
 
 
 
@@ -200,20 +124,6 @@ function scrollHoriz(val){
 }
 
 
-/* for(let i=0; i<divHaveStoryAndName.length; i++){
-    divHaveStoryAndName[i].addEventListener("click",()=>{
-        main.style.display = "none";
-        header.style.display = "none";
-        theStroyOpen.style.display = "flex";
-    })
-}
-
-closeStoryOpen.addEventListener("click", ()=>{
-    theStroyOpen.style.display = "none";
-    main.style.display = "flex";
-    header.style.display = "flex";
-})
- */
 
 
 
